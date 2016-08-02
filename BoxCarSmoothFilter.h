ea5f4c41fd07ce6a1c2da8b94146b60e42154d6a
Tom Wright <thomas.wright@tiscali.co.uk>
2016-08-02 20:51:18 +0100

    Playing around with threading implementation to see what happens to performance

--- BoxCarSmoothFilter.h
+++ BoxCarSmoothFilter.h
@@ -11,6 +11,14 @@
 
 #include <itkImageToImageFilter.h>
 
+// Forward declarations
+namespace itk
+{
+    class TimeProbe;
+}
+
+#define USE_THREADED_IMPLEMENTATION 1
+
 /**
  * Simple box-car filter implementation using a 3x3x3 voxel kernel.
  */
@@ -24,6 +32,9 @@ public:
     typedef itk::SmartPointer<Self> Pointer;
     typedef itk::SmartPointer<const Self> ConstPointer;
     
+    /** Superclass typedefs. */
+    typedef typename Superclass::OutputImageRegionType OutputImageRegionType;
+
     /** Method for creation through the object factory. */
     itkNewMacro(Self);
     
@@ -33,10 +44,16 @@ public:
     // Optional method for output
     void PrintSelf( std::ostream& os, itk::Indent indent ) const;
 
+#if !USE_THREADED_IMPLEMENTATION
     // Because we need neighbouring pixels to do the processing, we'll create our own implementation
     // of this method.
     virtual void GenerateData() ITK_OVERRIDE;
-
+#else
+    virtual void BeforeThreadedGenerateData() ITK_OVERRIDE;
+    virtual void AfterThreadedGenerateData() ITK_OVERRIDE;
+    virtual void ThreadedGenerateData( const OutputImageRegionType& outputRegionForThread, itk::ThreadIdType threadId ) ITK_OVERRIDE;
+#endif
+    
 protected:
     
     BoxCarSmoothFilter() {};
@@ -47,6 +64,7 @@ private:
     BoxCarSmoothFilter(const Self &) ITK_DELETE_FUNCTION;
     void operator=(const Self &) ITK_DELETE_FUNCTION;
     
+    itk::TimeProbe* clock;
 };
 
 #ifndef ITK_MANUAL_INSTANTIATION
--- BoxCarSmoothFilter.hxx
+++ BoxCarSmoothFilter.hxx
@@ -14,6 +14,7 @@
 #include "itkNeighborhoodIterator.h"
 #include "itkImageIterator.h"
 #include "itkTimeProbe.h"
+#include "itkConstantBoundaryCondition.h"
 
 template< typename TImage >
 void BoxCarSmoothFilter< TImage >::PrintSelf( std::ostream& os, itk::Indent indent ) const
@@ -21,6 +22,7 @@ void BoxCarSmoothFilter< TImage >::PrintSelf( std::ostream& os, itk::Indent inde
     os << "I am a box-car filter using a 3x3x3 voxel kernel, ignoring the outer edge of voxels" << std::endl;
 }
 
+#if !USE_THREADED_IMPLEMENTATION
 template< typename TImage >
 void BoxCarSmoothFilter< TImage >::GenerateData()
 {
@@ -33,6 +35,8 @@ void BoxCarSmoothFilter< TImage >::GenerateData()
     
     this->AllocateOutputs();
 
+#define USE_NEIGHBOURHOOD_ITERATOR 0
+#if USE_NEIGHBOURHOOD_ITERATOR
     // Set up neighbourhood iterator for iterating over the input
     typedef itk::ConstNeighborhoodIterator< TImage > NeighborhoodIteratorType;
     typename NeighborhoodIteratorType::RadiusType radius;
@@ -54,48 +58,103 @@ void BoxCarSmoothFilter< TImage >::GenerateData()
         const typename TImage::PixelType filteredValue = static_cast< typename TImage::PixelType >( accumulator /= inputIt.Size() );
         outputIt.Set( filteredValue );
     }
-//    const typename TImage::RegionType & outputRegion = output->GetRequestedRegion();
-//    typename TImage::IndexType start = outputRegion.GetIndex();
-//    typename TImage::SizeType size = outputRegion.GetSize();
-//    for ( int k = start[2] + 1; k < start[2] + size[2] - 1; ++k )
-//    {
-//        for ( int j = start[1] + 1; j < start[1] + size[1] - 1; ++j )
-//        {
-//            for ( int i = start[0] + 1; i < start[0] + size[0] - 1; ++i )
-//            {
-//                // Loop over the kernel on our input image
-//                float accumulator = 0;
-//                for ( int kk = k - 1; kk <= k + 1; ++kk )
-//                {
-//                    for ( int jj = j - 1; jj <= j + 1; ++jj )
-//                    {
-//                        for ( int ii = i - 1; ii <= i + 1; ++ii )
-//                        {
-//                            typename TImage::IndexType inPos;
-//                            inPos[0] = ii;
-//                            inPos[1] = jj;
-//                            inPos[2] = kk;
-//                            const float inputValue = input->GetPixel( inPos );
-//                            accumulator += inputValue;
-//                        }
-//                    }
-//                }
-//                
-//                // Output voxel index
-//                typename TImage::IndexType outPos;
-//                outPos[0] = i;
-//                outPos[1] = j;
-//                outPos[2] = k;
-//                
-//                // Average the accumulator and store
-//                const typename TImage::PixelType filteredValue = static_cast< typename TImage::PixelType >( accumulator /= 27.0f );
-//                output->SetPixel( outPos, filteredValue );
-//            }
-//        }
-//    }
+#else
+    const typename TImage::RegionType & outputRegion = output->GetRequestedRegion();
+    typename TImage::IndexType start = outputRegion.GetIndex();
+    typename TImage::SizeType size = outputRegion.GetSize();
+    for ( int k = start[2] + 1; k < start[2] + size[2] - 1; ++k )
+    {
+        for ( int j = start[1] + 1; j < start[1] + size[1] - 1; ++j )
+        {
+            for ( int i = start[0] + 1; i < start[0] + size[0] - 1; ++i )
+            {
+                // Loop over the kernel on our input image
+                float accumulator = 0;
+                for ( int kk = k - 1; kk <= k + 1; ++kk )
+                {
+                    for ( int jj = j - 1; jj <= j + 1; ++jj )
+                    {
+                        for ( int ii = i - 1; ii <= i + 1; ++ii )
+                        {
+                            typename TImage::IndexType inPos;
+                            inPos[0] = ii;
+                            inPos[1] = jj;
+                            inPos[2] = kk;
+                            const float inputValue = input->GetPixel( inPos );
+                            accumulator += inputValue;
+                        }
+                    }
+                }
+                
+                // Output voxel index
+                typename TImage::IndexType outPos;
+                outPos[0] = i;
+                outPos[1] = j;
+                outPos[2] = k;
+                
+                // Average the accumulator and store
+                const typename TImage::PixelType filteredValue = static_cast< typename TImage::PixelType >( accumulator /= 27.0f );
+                output->SetPixel( outPos, filteredValue );
+            }
+        }
+    }
+#endif
     
     clock.Stop();
-    std::cout << "Total: " << clock.GetTotal() << std::endl;
+    std::cout << "Total time for box car filtering: " << clock.GetTotal() << std::endl;
 }
+
+#else
+
+template< typename TImage >
+void BoxCarSmoothFilter< TImage >::BeforeThreadedGenerateData()
+{
+    this->clock = new itk::TimeProbe();
+    this->clock->Start();
+}
+
+template< typename TImage >
+void BoxCarSmoothFilter< TImage >::AfterThreadedGenerateData()
+{
+    this->clock->Stop();
+    std::cout << "Total time for box car filtering (threaded): " << this->clock->GetTotal() << std::endl;
+    delete this->clock;
+    this->clock = nullptr;
+}
+
+template< typename TImage >
+void BoxCarSmoothFilter< TImage >::ThreadedGenerateData( const OutputImageRegionType& outputRegionForThread, itk::ThreadIdType threadId )
+{
+    typename TImage::ConstPointer input = this->GetInput();
+    typename TImage::Pointer output = this->GetOutput();
+    
+    this->AllocateOutputs();
+    
+    //// TGW massive assumption that input region and output region are the same
+    
+    // Set up neighbourhood iterator for iterating over the input
+    typedef itk::ConstNeighborhoodIterator< TImage > NeighborhoodIteratorType;
+    typename NeighborhoodIteratorType::RadiusType radius;
+    radius.Fill(1);
+    NeighborhoodIteratorType inputIt(radius, input, outputRegionForThread);
+    
+    // Normal iterator for iterating over the output
+    typedef itk::ImageRegionIterator< TImage > IteratorType;
+    IteratorType outputIt( output, outputRegionForThread);
+    
+    // Now loop!
+    for ( inputIt.GoToBegin(), outputIt.GoToBegin(); !inputIt.IsAtEnd(); ++inputIt, ++outputIt )
+    {
+        float accumulator = 0;
+        for ( int kk = 0; kk < inputIt.Size(); ++kk )
+        {
+            accumulator += inputIt.GetPixel( kk );
+        }
+        const typename TImage::PixelType filteredValue = static_cast< typename TImage::PixelType >( accumulator /= inputIt.Size() );
+        outputIt.Set( filteredValue );
+    }
+}
+
+#endif
 
 #endif /* BoxCarSmoothFilter_h */
